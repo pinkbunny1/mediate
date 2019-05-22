b6208cac6270ae52a32148d95cbd04f0f771d617
Hoang Le <e1500973@edu.vamk.fi>
2019-05-22 20:31:17 +0300

    Revert "Hoang branch -> master"

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-const express = require("express");
-const axios = require('axios');
+const express = require('express');
+
 const app = express();
 
-app.get("/", (req, res) => {
-    axios.get('https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=avenger&api-key=KcivkecbgfUqo4J7DgHNdMtw5XgZO0Ey')
-    .then(function (response) {
-        // handle success
-        console.log(response.data)
-        res.json(response.data)
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-});
+app.get('/', (req, res) => res.send({ message: 'Hello from my app!' }));
 
 // Important: port must be read from the environment for Heroku
 const port = process.env.PORT || 3000;
